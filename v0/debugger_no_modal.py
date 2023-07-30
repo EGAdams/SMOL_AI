78859829cf3f645a251b12cad020b802e17e1fe6
egadams <eg1972@gmail.com>
2023-07-30 15:48:09 -0400

    commit before make rewrite

--- v0/debugger_no_modal.py
+++ v0/debugger_no_modal.py
@@ -1,25 +1,15 @@
-import sys
 import os
 from time import sleep
-from constants import DEFAULT_DIR, DEFAULT_MODEL, DEFAULT_MAX_TOKENS, EXTENSION_TO_SKIP
-import argparse
+DEFAULT_DIR             = "/home/adamsl/linuxBash/SMOL_AI/tennis_unit_tests/"
+OBJECT_OF_INTEREST      = "PinInterface"
+TURBO_MODEL             = "gpt-3.5-turbo"
+
 def read_file(filename):
-    with open(filename, "r") as file:
+    with open(filename, "r" ) as file:
         return file.read()
 
-
 def walk_directory(directory):
-    image_extensions = [
-        ".png",
-        ".jpg",
-        ".jpeg",
-        ".gif",
-        ".bmp",
-        ".svg",
-        ".ico",
-        ".tif",
-        ".tiff",
-    ]
+    image_extensions = [ ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".ico", ".tif", ".tiff" ]
     code_contents = {}
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -37,41 +27,27 @@ def walk_directory(directory):
                     ] = f"Error reading file {file}: {str(e)}"
     return code_contents
 
+def main():
+    prompt          = read_file( DEFAULT_DIR + "make_error_prompt.md" ) # the error message
+    directory       = DEFAULT_DIR + OBJECT_OF_INTEREST                  # the object's directory
+    model           = TURBO_MODEL                                       # the model to use
+    code_contents   = walk_directory( directory ) # code_contents will contain all of the code.
+    context         = "\n".join( f"{path}:\n{contents}" for path, contents in code_contents.items())
+    system          = "You are an AI debugger who is trying to debug a program for a user based on their file system. The user has provided you with the following files and their contents, finally folllowed by the error message or issue they are facing."
 
-def main(args):
-    prompt=args.prompt
-    directory= args.directory
-    model=args.model
-    code_contents = walk_directory(directory)
+    prompt =  ( "My files are as follows:\n```\n" + context + "```\n\n" + "My issue is as follows: " + prompt )
+    prompt += ( "\n\nAct as a world-class, expert C++ project debugger and my helpful assistant.  Please rewrite the Makefile as necessary." )
+    
+    with open( "temp.md", "w" ) as file: # write prompt contents into temp.md
+        file.write( prompt )
 
-    # Now, `code_contents` is a dictionary that contains the content of all your non-image files
-    # You can send this to OpenAI's text-davinci-003 for help
-
-    context = "\n".join(
-        f"{path}:\n{contents}" for path, contents in code_contents.items()
-    )
-    system = "You are an AI debugger who is trying to debug a program for a user based on their file system. The user has provided you with the following files and their contents, finally folllowed by the error message or issue they are facing."
-    prompt = (
-        "My files are as follows: "
-        + context
-        + "\n\n"
-        + "My issue is as follows: "
-        + prompt
-    )
-    prompt += (
-        "\n\nGive me ideas for what could be wrong and what fixes to do in which files."
-    )
-    res = generate_response(system, prompt, model)
-    # print res in teal
-    print("\033[96m" + res + "\033[0m")
-
-
-def generate_response(system_prompt, user_prompt, model=DEFAULT_MODEL, *args):
+    print( "\033[96m" + generate_response( system, prompt, model ) + "\033[0m" ) # print res in teal
+    # delete temp.md
+    os.remove( "temp.md" )
+    
+def generate_response(system_prompt, user_prompt, model=TURBO_MODEL, *args):
     import openai
-
-    # Set up your OpenAI API credentials
-    openai.api_key = os.environ["OPENAI_API_KEY"]
-
+    openai.api_key = os.environ[ "OPENAI_API_KEY" ] # Set up your OpenAI API credentials
     messages = []
     messages.append({"role": "system", "content": system_prompt})
     messages.append({"role": "user", "content": user_prompt})
@@ -80,50 +56,19 @@ def generate_response(system_prompt, user_prompt, model=DEFAULT_MODEL, *args):
     for value in args:
         messages.append({"role": role, "content": value})
         role = "user" if role == "assistant" else "assistant"
-
-    params = {
-        "model": model,
-        # "model": "gpt-4",
-        "messages": messages,
-        "max_tokens": 1500,
-        "temperature": 0,
-    }
-
-    # Send the API request
+    params = { "model": model, "messages": messages, "max_tokens": 1500, "temperature": 0 }
     keep_trying = True
-    while keep_trying:
+    while keep_trying:  # Send the API request
         try:
-            response = openai.ChatCompletion.create(**params)
+            response = openai.ChatCompletion.create( **params )
             keep_trying = False
         except Exception as e:
             # e.g. when the API is too busy, we don't want to fail everything
-            print("Failed to generate response. Error: ", e)
-            sleep(30)
-            print("Retrying...")
-
-    # Get the reply from the API response
-    reply = response.choices[0]["message"]["content"]
+            print( "Failed to generate response. Error: ", e)
+            sleep( 30 )
+            print( "Retrying..." )
+    reply = response.choices[ 0 ][ "message" ][ "content" ] # Get the reply from the API response
     return reply
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "prompt",
-        help="The prompt to use for the AI. This should be the error message or issue you are facing.",
-        
-    )
-    parser.add_argument(
-        "--directory",
-        "-d",
-        help="The directory to use for the AI. This should be the directory containing the files you want to debug.",
-        default=DEFAULT_DIR,
-    )
-    parser.add_argument(
-        "--model",
-        "-m",
-        help="The model to use for the AI. This should be the model ID of the model you want to use.",
-        default=DEFAULT_MODEL,
-    )
-    args = parser.parse_args()
-    main(args)
+    main()
